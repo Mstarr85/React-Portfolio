1fcab4335fe3f2f391e2fd5e446a8638ae21f7d3
Matthew Starr <buylowsellhigh@me.com>
2020-09-01 08:12:07 -0400

    Set up 3 functioning dummy cards to place projects in

--- react-portfolio/src/components/projects.js
+++ react-portfolio/src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl';
 
 
 class Projects extends Component {
@@ -12,7 +12,62 @@ class Projects extends Component {
 
         if(this.state.activeTab === 0){
             return(
-                <div><h1>This is React</h1></div>
+                <div className="projects-grid">
+
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                   <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg.webp) center / cover'}} >React Project #1</CardTitle>
+                   <CardText>
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   </CardText>
+                   <CardActions border>
+                       <Button colored>Github</Button>
+                       <Button colored>CodePen</Button>
+                       <Button colored>LiveDemo</Button>
+                   </CardActions>
+                   <CardMenu style={{color: '#fff'}}>
+                       <IconButton name="share" />
+                   </CardMenu>
+
+               </Card>
+
+                {/* Project 2 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg.webp) center / cover'}} >React Project #2</CardTitle>
+                <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                    <Button colored>LiveDemo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                    <IconButton name="share" />
+                </CardMenu>
+
+            </Card>
+
+             {/* Project 3 */}
+             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg.webp) center / cover'}} >React Project #3</CardTitle>
+             <CardText>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             </CardText>
+             <CardActions border>
+                 <Button colored>Github</Button>
+                 <Button colored>CodePen</Button>
+                 <Button colored>LiveDemo</Button>
+             </CardActions>
+             <CardMenu style={{color: '#fff'}}>
+                 <IconButton name="share" />
+             </CardMenu>
+
+         </Card>
+
+         
+                </div>
+                
             )
             } else if(this.state.activeTab === 1){
                 return (
@@ -43,9 +98,13 @@ class Projects extends Component {
                     <Tab>Javascript</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
-                    {this.toggleCategories()}
-                </section>
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+                   
+        
                 </div>
         )
     }
