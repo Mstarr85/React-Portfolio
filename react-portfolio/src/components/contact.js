2e74640c2de2a24a3483159023aa113099388041
Matthew Starr <buylowsellhigh@me.com>
2020-09-30 09:35:57 -0400

    Making sure everything is saved before I add a form

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -3,6 +3,8 @@ import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 import Avatar from '/Users/matthew/Desktop/React-Portfolio/react-portfolio/src/myAvatar.png';
 
 
+
+
 class Contact extends Component {
     render() {
         return(
@@ -15,7 +17,7 @@ class Contact extends Component {
                        alt="avatar"
                        style={{height: '250px'}}
                        />
-                       <p style={{ width: '75', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                       <p style={{ width: '75', margin: 'auto', paddingTop: '1em'}}> </p>
                    </Cell>
                    <Cell col={6}>
                        <h2>Contact Me</h2>
@@ -26,21 +28,14 @@ class Contact extends Component {
                             <ListItem>
                                 <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
                                     <i className="fa fa-phone-square" aria-hidden="true"/>
-                                    (123) 456-7890
-                                    </ListItemContent>
-                             </ListItem>
-
-                             <ListItem>
-                                <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
-                                    <i className="fa fa-fax" aria-hidden="true"/>
-                                    (123) 456-7890
+                                    (704) 443-1053
                                     </ListItemContent>
                              </ListItem>
 
                              <ListItem>
                                 <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
                                     <i className="fa fa-envelope" aria-hidden="true"/>
-                                    someone@example.com
+                                    buylowsellhigh@me.com
                                     </ListItemContent>
                              </ListItem>
 
@@ -52,6 +47,9 @@ class Contact extends Component {
                              </ListItem>
                         </List>
                         </div>
+
+
+
                         </Cell>
                     </Grid>
                 </div>
