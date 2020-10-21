6c6b385c1be3dc9b82c569a9b3426e27fa591a22
Matthew Starr <buylowsellhigh@me.com>
2020-10-21 11:51:38 -0400

    added text and made text and contact icons clickable on contact.js

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -20,10 +20,23 @@ class Contact extends Component {
                        <img
                        src={Avatar}
                        alt="avatar"
-                       style={{height: '250px', marginTop: '5em', borderRadius: '50%'}}
+                       style={{height: '250px', marginTop: '5em', marginBottom: '5em', borderRadius: '50%'}}
                        />
                        <p style={{ width: '75', margin: 'auto', paddingTop: '1em'}}> </p>
+
+                    <div>
+                    <h3 style={{fontFamily: 'Anton'}}>I'd love to hear from you!</h3>
+                    <p style={{width:'75%', margin:'auto', fontSize: '20px', fontFamily: 'roboto'}}>I'm happy to answer any questions you may have or provide you with an estimate for your current project. Just send me a message in the contact form or click on the contact links provided to the right of the page. </p> 
+                    <br/>
+                    <h4 style={{fontFamily: 'Anton'}}>Hope to hear from you soon! </h4>   
+                    </div>
+
                    </Cell>
+
+
+  
+
+
                    <Cell col={6}>
                        <h2>Contact Me</h2>
                        <hr/>
@@ -32,23 +45,23 @@ class Contact extends Component {
                         <List>
                             <ListItem>
                                 <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
-                                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                                    <a style={{color: 'black',textDecoration: 'none'}} href="tel:+7044431053">(704) 443-1053</a>
-                                 
-                                    </ListItemContent>
+                                <a style={{color: 'black',textDecoration: 'none'}} href="tel:+7044431053">
+                                    <i className="fa fa-phone-square" aria-hidden="true"/> (704) 443-1053</a>
+                                   </ListItemContent>
                              </ListItem>
 
                              <ListItem>
                                 <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
-                                    <i className="fa fa-envelope" aria-hidden="true"/>
-                                    <a  style={{color: 'black',textDecoration: 'none'}} href="mailto:buylowsellhigh@me.com">buylowsellhigh@me.com</a>
+                                    <a  style={{color: 'black',textDecoration: 'none'}} href="mailto:buylowsellhigh@me.com">
+                                        <i className="fa fa-envelope" aria-hidden="true"/>buylowsellhigh@me.com</a>
+
                                     </ListItemContent>
                              </ListItem>
 
                              <ListItem>
                                 <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
                                 <a  style={{color: 'black',textDecoration: 'none'}} href="https://www.linkedin.com/in/matthew-starr-a9b28b126/" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin-square" aria-hidden="true" />LinkedIn Profile
+                                <i className="fa fa-linkedin-square" aria-hidden="true"/> LinkedIn Profile
                             </a>
                                     </ListItemContent>
                              </ListItem>
